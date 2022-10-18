5014d1ac0b0827628ff57d0074bab51c3c073d1b
sigristarisa <arisasigrist.ch@gmail.com>
2022-10-18 14:59:42 +0200

    created diamond only with css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import Diamond from "./components/Diamond/Diamond";
+
 function App() {
   return (
     <div className='App'>
-      <div>hi</div>
+      <Diamond />
     </div>
   );
 }
---
+++ src/components/Diamond/Diamond.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Diamond.css";
+
+const Diamond: React.FC = () => {
+  return (
+    <div className='diamond'>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Diamond;
