3dfc8879169a0c4e742cf3fb0b4f3985fb829be5
sigristarisa <arisasigrist.ch@gmail.com>
2022-10-18 16:15:33 +0200

    made Pepsi logo purely with css

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Diamond from "./components/Diamond/Diamond";
 import Envelope from "./components/Envelope/Envelope";
+import Pepsi from "./components/Pepsi/Pepsi";
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <Diamond />
       <Envelope />
+      <Pepsi />
     </div>
   );
 };
---
+++ src/components/Pepsi/Pepsi.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Pepsi.css";
+
+const Pepsi: React.FC = () => {
+  return <div className='pepsi'></div>;
+};
+
+export default Pepsi;
