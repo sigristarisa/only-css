05ebfe8133d7a101f3f0b38eb468ffa510c25e3a
sigristarisa <arisasigrist.ch@gmail.com>
2022-10-19 16:39:34 +0200

    created IG logo

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import CaptainAmerica from "./components/CaptainAmerica/CaptainAmerica";
 import Gear from "./components/Gear/Gear";
 import GitLogo from "./components/GitLogo/GitLogo";
 import GoogleLogo from "./components/GoogleLogo/GoogleLogo";
+import IGLogo from "./components/IGLogo/IGLogo";
 
 const App: React.FC = () => {
   return (
@@ -18,12 +19,13 @@ const App: React.FC = () => {
       <Envelope />
       <Pepsi />
       <Camera />
-      <GoogleLogo />
+      <IGLogo />
       <CoffeeMug />
       <Batman />
       <CaptainAmerica />
       <Gear />
       <GitLogo />
+      <GoogleLogo />
     </div>
   );
 };
---
+++ src/components/IGLogo/IGLogo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./IGLogo.css";
+
+const IGLogo: React.FC = () => {
+  return (
+    <div className='ig-logo'>
+      <div className='ig-square'></div>
+      <div className='ig-circle'></div>
+      <div className='ig-flash'></div>
+    </div>
+  );
+};
+
+export default IGLogo;
