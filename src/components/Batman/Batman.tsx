89e186cb758e425ef3f036ea6f44e25f44ca646b
sigristarisa <arisasigrist.ch@gmail.com>
2022-10-19 11:05:01 +0200

    created Batman logo

---
+++ src/components/Batman/Batman.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Batman.css";
+
+const Batman: React.FC = () => {
+  return (
+    <div className='oval'>
+      <div className='logo'>
+        <div className='o1'></div>
+        <div className='o2'> </div>
+        <div className='sq1'></div>
+        <div className='sq2'></div>
+        <div className='ears'></div>
+        <div className='o3'> </div>
+        <div className='o4'> </div>
+        <div className='o5'> </div>
+        <div className='o6'> </div>
+      </div>
+    </div>
+  );
+};
+
+export default Batman;
