b5532b3a4e03ac3a558eefdc10b1250893b134ae
sigristarisa <arisasigrist.ch@gmail.com>
2022-10-24 16:43:41 +0200

    created React logo

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import IGLogo from "./components/IGLogo/IGLogo";
 import Panda from "./components/Panda/Panda";
 import Donut from "./components/Donut/Donut";
 import FigmaLogo from "./components/FigmaLogo/FigmaLogo";
+import ReactLogo from "./components/ReactLogo/ReactLogo";
 
 const App: React.FC = () => {
   return (
@@ -25,13 +26,14 @@ const App: React.FC = () => {
       <CoffeeMug />
       <Batman />
       <CaptainAmerica />
-      <FigmaLogo />
       <Panda />
       <Gear />
+      <ReactLogo />
       <GitLogo />
       <GoogleLogo />
       <IGLogo />
       <Donut />
+      <FigmaLogo />
     </div>
   );
 };
---
+++ src/components/ReactLogo/ReactLogo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ReactLogo.css";
+
+const ReactLogo: React.FC = () => {
+  return (
+    <div className='react'>
+      <div className='react-center'></div>
+      <div className='react-oval'></div>
+      <div className='react-oval ro-left'></div>
+      <div className='react-oval ro-right'></div>
+    </div>
+  );
+};
+
+export default ReactLogo;
